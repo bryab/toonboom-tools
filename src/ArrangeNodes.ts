2d7c5e1938524b1f0a7ad7cb66a3687031d1f8de
Bryan Fordney <126733+bryab@users.noreply.github.com>
2025-03-27 15:46:32 -0400

    create MLW from selected nodes

--- src/ArrangeNodes.ts
+++ src/ArrangeNodes.ts
@@ -3,8 +3,8 @@
 function StackNodes() {
   scene.beginUndoRedoAccum("Arrange selected nodes in a stack.");
   var nodes = selection.selectedNodes();
-  Utils.sortNodesByY(nodes);
-  Utils.arrangeTable(nodes, { num_cols: 1, spacing_y: 10 });
+  NodeUtils.sortNodesByY(nodes);
+  NodeUtils.arrangeTable(nodes, { num_cols: 1, spacing_y: 10 });
   scene.endUndoRedoAccum();
 }
 
@@ -24,7 +24,7 @@ function AlignHorizontal() {
   const avg = sum / all_y.length;
 
   modules.forEach(function (nodePath) {
-    Utils.move(
+    NodeUtils.move(
       nodePath,
       node.coordX(nodePath),
       avg - node.height(nodePath) / 2
@@ -33,7 +33,185 @@ function AlignHorizontal() {
   scene.endUndoRedoAccum();
 }
 
-namespace Utils {
+function LinkToMultiLayerWrite() {
+  const nodes = selection.selectedNodes();
+
+  if (!nodes.length) {
+    return;
+  }
+
+  let mlw = NodeUtils.findNode(
+    (nodePath) => (node.type(nodePath) as string) == "MultiLayerWrite", // FIXME node.type return type should just be string
+    node.root(),
+    false
+  );
+
+  scene.beginUndoRedoAccum("Connect to MLW");
+
+  if (!mlw) {
+    const coord = NodeUtils.coordNear(nodes[0]);
+    mlw = node.add(
+      node.root(),
+      "Multi-Layer-Write",
+      "MultiLayerWrite",
+      coord.x,
+      coord.y,
+      0
+    );
+  }
+
+  for (const nodePath of nodes) {
+    for (let i = 0; i < node.numberOfOutputPorts(nodePath); i++) {
+      let found = false;
+      for (let l = 0; l < node.numberOfOutputLinks(nodePath, i); l++) {
+        if (node.dstNode(nodePath, i, l) == mlw) {
+          found = true;
+        }
+      }
+      if (!found) {
+        WriteUtils.ConnectToMLW(nodePath, i, mlw);
+      }
+    }
+  }
+
+  scene.endUndoRedoAccum();
+}
+
+namespace WriteUtils {
+  export function ConnectToMLW(srcNode: string, outPort: number, mlw: string) {
+    const srcName = node.getName(srcNode);
+    let create_comp = false;
+    let group = node.parentNode(srcNode);
+    MessageLog.trace(`Node: ${srcNode} Group: ${group}`);
+
+    while (group != "Top") {
+      create_comp = true;
+      MessageLog.trace(`Wiring out of group: ${group}`);
+      const out_node = node.getGroupOutputModule(
+        group,
+        "Multi-Port_Out",
+        0,
+        0,
+        0
+      );
+      const multi_out_port = node.numberOfInputPorts(out_node);
+      MessageLog.trace(
+        `Linking from ${srcNode}:${outPort} to ${out_node}:${multi_out_port}`
+      );
+      node.link(srcNode, outPort, out_node, multi_out_port);
+      srcNode = group;
+      outPort = multi_out_port;
+      group = node.parentNode(srcNode);
+    }
+
+    if (create_comp) {
+      const coord = NodeUtils.coordNear(srcNode);
+      const comp = node.add(
+        node.root(),
+        srcName,
+        "COMPOSITE",
+        coord.x,
+        coord.y,
+        0
+      );
+      node.link(srcNode, outPort, comp, 0);
+      srcNode = comp;
+      outPort = 0;
+    }
+    const mlw_port = node.numberOfInputPorts(mlw);
+
+    node.link(srcNode, outPort, mlw, mlw_port);
+  }
+}
+
+namespace NodeUtils {
+  type NodeCallback = (nodePath: string) => boolean | void;
+
+  export function findNode(
+    filter: (nodePath: string) => boolean,
+    root?: string,
+    recurse = true
+  ): string {
+    let found: string = null;
+
+    forEachNodeAlt(
+      (nodePath) => {
+        if (filter(nodePath)) {
+          found = nodePath;
+          return true;
+        }
+      },
+      root,
+      recurse
+    );
+
+    return found;
+  }
+
+  export function forEachNodeAlt(
+    callback: (nodePath: string) => boolean,
+    root?: string,
+    recurse = true
+  ): boolean {
+    const groups: string[] = [];
+
+    /**
+     * First search everything in this group.
+     * Only recurse after that.
+     */
+
+    let found = false;
+
+    forEachNode(
+      (nodePath) => {
+        if (callback(nodePath)) {
+          found = true;
+        } else if (recurse && node.isGroup(nodePath)) {
+          groups.push(nodePath);
+        }
+      },
+      root,
+      false
+    );
+
+    if (found) return true;
+
+    if (recurse) {
+      for (let groupName of groups) {
+        // MessageLog.trace(`Recursing into... ${groupName}`);
+        const found = forEachNodeAlt(callback, groupName, true);
+        if (found) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  export function forEachNode(
+    callback: NodeCallback,
+    groupName?: string,
+    recurse = true
+  ) {
+    if (groupName === undefined) groupName = node.root();
+    const num_children = node.numberOfSubNodes(groupName);
+    for (let i = 0; i < num_children; i++) {
+      const child = node.subNode(groupName, i);
+      if (callback(child)) return;
+      if (recurse && node.isGroup(child)) forEachNode(callback, child);
+    }
+  }
+
+  export function coordNear(srcnode: string): Point2d {
+    const offset_x = 0;
+    const offset_y = 50;
+    return new Point2d(
+      node.coordX(srcnode) + offset_x,
+      node.coordY(srcnode) + offset_y
+    );
+  }
+
   export function sortNodesByY(nodes: string[]) {
     return nodes.sort(function (a, b) {
       const y1 = node.coordY(a);
